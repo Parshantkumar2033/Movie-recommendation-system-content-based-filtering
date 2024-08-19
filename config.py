654520b5e67e39713686475c869ca36a691bc742
Parshantkumar2033 <125487613+Parshantkumar2033@users.noreply.github.com>
2024-08-20 02:34:00 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-TRAININGFILE_CREDIT = "path to the credits dataset"
-TRAININGFILE_MOVIE = "path to the movies dataset"
-
-MOVIES_PKL = "path to Movies.pkl"
-SIMILARITY_PKL = "path to Similarity.pkl"
-
-APIREQUEST = "https://api.themoviedb.org/3/movie/{}?api_key=YOUR_API_KEY"
