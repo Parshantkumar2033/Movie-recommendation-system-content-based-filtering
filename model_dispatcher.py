cecadb5c3a5638149efa0baabc2df80fb7a9e5ee
Parshantkumar2033 <125487613+Parshantkumar2033@users.noreply.github.com>
2024-08-20 02:34:33 +0530

    Delete model_dispatcher.py

--- model_dispatcher.py
+++
@@ -1,8 +0,0 @@
-import nltk.stem.porter
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem.porter import PorterStemmer
-
-models = {
-    'cVec' : CountVectorizer(max_features= 5000, stop_words= 'english'),
-    'porterStemmer' : PorterStemmer()
-}
